840258128f7bf3979a09899dcdfcf681b2647a13
Jitin Singh <jitinsingh000@gmail.com>
2023-03-06 20:42:09 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function myFunction() {
-    var x = document.getElementById("navbar-ul");
-    if (x.style.display === "block") {
+    let x = document.getElementById("navbar-ul");
+    let n = document.getElementById("navbar");
+    // if (x.style.display === "block") {
+    if (x.style.display === "flex") {
         x.style.display = "none";
+        
     } else {
-        x.style.display = "block";
+        // x.style.display = "block";
+        x.style.display = "flex";
     }
 }
